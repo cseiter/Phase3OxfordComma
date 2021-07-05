1c523b6a99a24f0249e287399de5100256240b11
chris <cseiter@gmail.com>
2021-07-05 15:51:44 -0400

    all tests passed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    if array.count > 2 then
+        array[-1] = "and #{array.last}";
+        oxford_string = array.join(", ");
+    elsif array.count > 1 then
+        oxford_string = array.join(" and ");
+    else
+        oxford_string = array.join()
+    end
+    oxford_string
 end
+
+array = ["fiddleheads","okra","kohlrabi"]
+
+oxford_comma(array)
